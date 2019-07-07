c9ca4587f52a26c414e1ed09fc1f7131101c1d16
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-07 11:28:01 +0100

    Using activate life cycle hook in StickyNote class

--- src/sticky-notes.js
+++ src/sticky-notes.js
@@ -1,3 +1,7 @@
 export class StickyNote {
-    
+    /* This is the activate life cycle hook
+        This is used to access any parameters that are passed */
+    activate(params) {
+        this.note = params.note;
+    }
 }
